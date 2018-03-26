1e98ead3edb6e21796cd2466b2e04623f70ef44b
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-03-26 10:20:23 +0200

    Added basic test

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,43 +1,35 @@
 import forEach from 'callbag-for-each'
 import pipe from 'callbag-pipe'
+import subject from 'callbag-subject'
 
 import dropUntil from '../src'
-
-const noop = () => {}
-
-const makeSource = () => {
-  const ret = {}
-  ret.source = (start, sink) => {
-    if (start !== 0) return
-    ret.next = sink
-    sink(0, noop)
-  }
-  return ret
-}
 
 test('works', () => {
   const actual = []
   let next
 
-  const source = makeSource()
-  const notifier = makeSource()
+  const source = subject()
+  const notifier = subject()
 
   pipe(
-    source.source,
-    dropUntil(notifier.source),
+    source,
+    dropUntil(notifier),
     forEach(data => {
       actual.push(data)
     }),
   )
 
   return Promise.resolve()
-    .then(() => source.next(0))
-    .then(() => source.next(1))
-    .then(() => source.next(2))
-    .then(() => source.next(3))
-    .then(() => notifier.next(0))
-    .then(() => source.next(4))
-    .then(() => source.next(5))
-    .then(() => source.next(6))
-    .then(() => source.next(7))
+    .then(() => source(1, 0))
+    .then(() => source(1, 1))
+    .then(() => source(1, 2))
+    .then(() => source(1, 3))
+    .then(() => notifier(1, 0))
+    .then(() => source(1, 4))
+    .then(() => source(1, 5))
+    .then(() => source(1, 6))
+    .then(() => source(1, 7))
+    .then(() => {
+      expect(actual).toEqual([4, 5, 6, 7])
+    })
 })
